2993ca29f03bf76ac4bca7bdaeedfde30ddf71e0
Niloufar Asghari <nilouasghari22@gmail.com>
2020-02-06 10:02:23 +0330

    const port is added

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express=require('express');
 
 const app = express();
 
+const port=3000;
+
 app.get('/',(req,res) =>{
     res.send('Hi      !!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 });
 
-app.listen(3000,()=>
+app.listen(port,()=>
 {
-    console.log('server is running on port 3000');
+    console.log(`server is running on port ${port}`);//in() we using the template 
+
 });
