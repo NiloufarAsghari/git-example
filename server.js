bdddfd460ffaea2723ef0962a630ef8b9fdc5122
Niloufar Asghari <nilouasghari22@gmail.com>
2020-02-06 10:01:39 +0330

    const  port is added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app = express();
+
+app.get('/',(req,res) =>{
+    res.send('Hi      !!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+});
+
+app.listen(3000,()=>
+{
+    console.log('server is running on port 3000');
+});
